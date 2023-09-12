4799cd378c32fda5f425dc318123bf3b59c51f98
jhohan-val <valderramajhohan@gmail.com>
2023-09-12 06:52:28 -0500

    feat:se creo funcion

---
+++ funcion1.py
@@ -0,0 +1,8 @@
+#funcion que reciba el nombre de una persona y la salude 
+
+#Declarar 
+def saludarPersona(nombre):
+    print(f"Hola buenos dias dormilon {nombre}")
+
+#usar una funcion = llamarla 
+saludarPersona("Jhohan valderrama")
